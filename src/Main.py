cdc69812a1c04434a3d85f88b2f6a37e4b14e2e2
B-Pence <73327227+B-Pence@users.noreply.github.com>
2025-01-04 21:20:12 -0600

    Fixed error handling and minor changes
    Corrected error handling to display proper error message, renamed ValidDirectory to ValidateDirectory, deleted whitespace, deleted redundant comments

--- src/Main.py
+++ src/Main.py
@@ -1,20 +1,18 @@
 import os
 
-def ValidDirectory(userDirectoryPath):
+def ValidateDirectory(userDirectoryPath):
     try:
         while os.path.isdir(userDirectoryPath) == False:
             userDirectoryPath = input("Please enter a valid directory: ")
-        # raise FileNotFoundError(f"{userDirectoryPath} directory not valid!")
+            
         if os.path.isdir(userDirectoryPath) == True:
             print(f"{userDirectoryPath} is a valid directory!")
-# except FileNotFoundError as e:
-#     print(e)
-    except Exception:
-        print(f"{Exception} unexpected error occurred.")
+
+    except Exception as e:
+        print(f"Error: {e} unexpected error occurred.")
 
 sourceDirectory = input("Please enter the directory to copy from: ")
-ValidDirectory(sourceDirectory)
+ValidateDirectory(sourceDirectory)
 
 destinationDirectory = input("Please enter the destination directory: ")
-ValidDirectory(destinationDirectory)
-
+ValidateDirectory(destinationDirectory)
