b2b488fbe351e5a4c8c332f113ca151f67e3f5a6
SoleStriker <114538195+SoleStriker@users.noreply.github.com>
2025-01-08 22:35:47 -0600

    WIP: Basic UI, initializing ui elements

--- src/Gui.py
+++ src/Gui.py
@@ -1,9 +1,26 @@
 import tkinter as tk
 
-window = tk.Tk("1000x1000")
+class GUI():
+    
+    def __init__(self):
+        self.window = tk.Tk("1920x1080")
 
-window.mainloop()
+        self.copyButton = tk.Button(self.window, text="Copy", font=("Fira Code", 16))
+        self.moveButton = tk.Button(self.window, text="Move", font=("Fira Code", 16))
+        self.copyButton.pack(padx=2, pady=1)
+        self.moveButton.pack(padx=2, pady=1)
+        
+        self.browseDestinationDirButton = tk.Button(self.window, text="Browse Destination", font=("Fira Code", 16))
+        self.browseDestinationDirButton.pack(padx=1, pady=2)
+        self.browseSourceDirButton = tk.Button(self.window, text="Browse Source", font=("Fira Code", 16))
+        self.browseSourceDirButton.pack(padx=1, pady=2)
+        
+        self.sourceDirectoryTextBox = tk.Text(self.window, height=5, font=("Fira Code", 16))
+        self.destinationDirectoryTextBox = tk.Text(self.window, height=5, font=("Fira Code", 16))
+        self.sourceDirectoryTextBox.pack(padx=.05, pady=3)
+        self.destinationDirectoryTextBox.pack(padx=1, pady=3)
+        
+        self.window.mainloop()
 
-
-
+GUI()
 
--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import Gui
 
 def ValidateDirectory(userDirectoryPath):
     try:
