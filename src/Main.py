a6fe14c24f6373f79113c1c280fa1aaf198ed060
Sole <114538195+makbaran@users.noreply.github.com>
2026-04-03 15:21:17 -0500

    Fix: Move operation now keeps source directory in tact whilst copying files and deleting them from source directory

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,22 @@
 import os
 import shutil
 
+def ChooseOperation():
+    operationState = ""
+    operationState = input("Enter the function you want to do.\n(C)opy or (M)ove: ")
+    # match operationState.lower():
+    #     case 'c': 
+    #         CopyDirectory(sourceDirectory, destinationDirectory)
+    #     case 'm':
+    #         MoveDirectory(sourceDirectory, destinationDirectory)
+    #     case _:
+    #         print("Error: Invalid input given!")
+
+    if operationState == "C" or "c":
+        CopyDirectory(sourceDirectory, destinationDirectory)
+    elif operationState == "M" or "m":
+        MoveDirectory(sourceDirectory, destinationDirectory)
+
 def ValidateDirectory(userDirectoryPath):
     try:
         while os.path.isdir(userDirectoryPath) == False:
@@ -9,12 +25,6 @@ def ValidateDirectory(userDirectoryPath):
             print(f"{userDirectoryPath} is a valid directory!")
     except Exception as e:
         print(f"Error: {e} unexpected error occurred.")
-
-sourceDirectory: str = input("Please enter the directory to copy from: ")
-ValidateDirectory(sourceDirectory)
-
-destinationDirectory: str = input("Please enter the destination directory: ")
-ValidateDirectory(destinationDirectory)
 
 def CopyDirectory(source, destination):
     try:
@@ -27,17 +37,23 @@ def CopyDirectory(source, destination):
 def MoveDirectory(source, destination):
     try:
         shutil.copytree(source, destination, dirs_exist_ok=True)
-        shutil.rmtree(source)
+        files = os.listdir(source)
+        for file in files:
+            filePath = os.path.join(source, file)
+            if os.path.join(filePath):
+                os.remove(filePath)
+            elif os.path.isdir(filePath):
+                shutil.rmtree(filePath)
+            
     except Exception as e:
         print(f"Error moving directory {source}: {e}")
     else:
         print(f"Moving directory {source} to {destination} was successful.")
 
-def ChooseOperation():
-    operationState = input("Enter the function you want to do.\n(C)opy or (M)ove: ")
-    if operationState == "C" or "c":
-        CopyDirectory(sourceDirectory, destinationDirectory)
-    elif operationState == "M" or "m":
-        MoveDirectory(sourceDirectory, destinationDirectory)
-        
+sourceDirectory: str = input("Please enter the source directory: ")
+ValidateDirectory(sourceDirectory)
+
+destinationDirectory: str = input("Please enter the destination directory: ")
+ValidateDirectory(destinationDirectory)
+
 ChooseOperation()
