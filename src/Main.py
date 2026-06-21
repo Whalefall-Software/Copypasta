4054a6445679d8ef0b71e75268ddb725c3b68099
Sole <114538195+makbaran@users.noreply.github.com>
2026-06-21 18:56:14 -0500

    Fix: Manual rebase with main branch

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+import filecmp
+import click
 import Gui
 
 Gui.launch()
@@ -10,18 +12,32 @@ def ValidateDirectory(userDirectoryPath):
             userDirectoryPath = input("Please enter a valid directory: ")
         if os.path.isdir(userDirectoryPath) == True:
             print(f"{userDirectoryPath} is a valid directory!")
+            totalBytes = GetSizeOfDirectory(userDirectoryPath)
     except Exception as e:
         print(f"Error: {e} unexpected error occurred.")
 
-sourceDirectory: str = input("Please enter the directory to copy from: ")
-ValidateDirectory(sourceDirectory)
-
-destinationDirectory: str = input("Please enter the destination directory: ")
-ValidateDirectory(destinationDirectory)
-
 def CopyDirectory(source, destination):
     try:
-        shutil.copytree(source, destination, dirs_exist_ok=True)
+        totalSize = GetSizeOfDirectory(source)
+        with click.progressbar(length=totalSize) as bar:
+
+            for dirPath, dirName, files in os.walk(source):
+
+                relativePath = os.path.relpath(dirPath, source)
+                targetPath = os.path.join(destination, relativePath)
+
+                os.makedirs(targetPath, exist_ok=True)
+
+                for file in files:
+
+                    sourceFile = os.path.join(dirPath, file)
+                    targetFile = os.path.join(targetPath, file)
+
+                    shutil.copy2(sourceFile, targetFile)
+
+                    fileSize = os.path.getsize(sourceFile)
+                    bar.update(fileSize)
+
     except Exception as e:
         print(f"Error copying directory {source}: {e}")
     else:
@@ -29,18 +45,90 @@ def CopyDirectory(source, destination):
 
 def MoveDirectory(source, destination):
     try:
-        shutil.copytree(source, destination, dirs_exist_ok=True)
-        shutil.rmtree(source)
+        totalSize = GetSizeOfDirectory(source)
+        with click.progressbar(length=totalSize) as bar:
+
+            for dirPath, dirName, files in os.walk(source):
+
+                relativePath = os.path.relpath(dirPath, source)
+                targetPath = os.path.join(destination, relativePath)
+
+                os.makedirs(targetPath, exist_ok=True)
+
+                for file in files:
+
+                    sourceFile = os.path.join(dirPath, file)
+                    targetFile = os.path.join(targetPath, file)
+
+                    shutil.copy2(sourceFile, targetFile)
+
+                    fileSize = os.path.getsize(sourceFile)
+                    bar.update(fileSize)
+
+        if not ValidateOperation(source, destination, False):
+            print("Copy failed...")
+            return
+
+        print("Cleaning up...")
+
+        files = os.listdir(source)
+        for file in files:
+            filePath = os.path.join(source, file)
+            if os.path.join(filePath):
+                os.remove(filePath)
+            elif os.path.isdir(filePath):
+                shutil.rmtree(filePath)
+            
     except Exception as e:
         print(f"Error moving directory {source}: {e}")
     else:
         print(f"Moving directory {source} to {destination} was successful.")
-
-def ChooseOperation():
-    operationState = input("Enter the function you want to do.\n(C)opy or (M)ove: ")
-    if operationState == "C":
-        CopyDirectory(sourceDirectory, destinationDirectory)
-    elif operationState == "M":
-        MoveDirectory(sourceDirectory, destinationDirectory)
         
-ChooseOperation()
+def ValidateOperation(source, destination, depth):
+    filecmp.clear_cache() # Used to eliminate cached comparison results that can give inaccurate results
+    try:
+        dirDiff = filecmp.dircmp(source, destination)
+        if dirDiff.diff_files:
+            print(f"Mismatched files: {dirDiff.diff_files}")
+            return False
+        elif dirDiff.left_only or dirDiff.right_only:
+            print(f"Only in source: {dirDiff.left_only}")
+            print(f"Only in destination: {dirDiff.right_only}")
+            return False
+        else:
+            print(f"Contents of contents of {source} to {destination} match.")
+            return True
+    except Exception as e:
+        print(f"Error validating contents of {source} to {destination} \n {e}")
+        
+def GetSizeOfDirectory(directory):
+    totalSize = 0
+    for dirPath, dirNames, fileNames in os.walk(directory):
+        for file in fileNames:
+            filePath = os.path.join(dirPath, file)
+            totalSize += os.path.getsize(filePath)
+
+    return totalSize
+   
+def ChooseOperation(sourceDirectory, destinationDirectory):
+    operationState = ""
+    operationState = input("Enter the function you want to do.\n(C)opy, (M)ove, (V)alidate, or (B)yte-by-byte validation: ")
+    match operationState.lower():
+        case 'c': 
+            CopyDirectory(sourceDirectory, destinationDirectory)
+        case 'm':
+            MoveDirectory(sourceDirectory, destinationDirectory)
+        case 'v':
+            ValidateOperation(sourceDirectory, destinationDirectory, False)
+        case 'b':
+            ValidateOperation(sourceDirectory, destinationDirectory, True)
+        case _:
+            print("Error: Invalid input given!")
+
+src: str = input("Please enter the source directory: ")
+ValidateDirectory(src)
+
+dst: str = input("Please enter the destination directory: ")
+ValidateDirectory(dst)
+
+ChooseOperation(src, dst)
